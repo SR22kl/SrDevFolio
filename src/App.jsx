efa18835063a45ce11479a28d4bab556d3c1452f
Sumit Rathod <sbrathod22kl@gmail.com>
2026-07-28 14:14:03 +0530

    logo & featurecards section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Hero from "./sections/Hero";
 import ShowCase from "./sections/ShowCase";
+import NavBar from "./components/NavBar";
+import LogoSection from "./sections/LogoSection";
+import FeatureCards from "./sections/FeatureCards";
+import ExperienceSection from "./sections/ExperienceSection";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
       <ShowCase />
+      <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
     </>
   );
 };
